b7dcea2cbea7155a4a26f46ed429162f6cae4c1b
Pia Mendoza <lmendoza22@my.bcit.ca>
2024-04-09 11:46:24 -0700

    updates in colours

---
+++ .history/components/MenuBar/index_20240409114538.js
@@ -0,0 +1,31 @@
+import styles from './MenuBar.module.css'
+
+export default function MenuBar() {
+    return (
+        <div className={styles.menubarContainer}>
+            <div className={styles.blurbg}>
+                <ul className={styles.menunames}>
+                    <li>
+                        <Link></Link><h5>Home</h5>
+                    </li>
+                    <li>
+                        <h5>Logos</h5>
+                    </li>
+                    <li>
+                        <h5>Typography</h5>
+                    </li>
+                    <li>
+                        <h5>Colours</h5>
+                    </li>
+                    <li>
+                        <h5>Layout</h5>
+                    </li>
+                    <li>
+                        <h5>Iconography</h5>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
